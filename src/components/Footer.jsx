c70e501fc4303d050b84889dffc3ce17014d7f6c
Aryan <aryan.gupta158935@gmail.com>
2026-01-18 16:11:39 +0530

    Update || Minimal changes in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,9 +6,9 @@ const Footer = () => {
           className="text-center text-gray-400 
           px-4 py-3 sm:py-4 
           text-xs sm:text-sm md:text-base 
-          hover:text-[#4DF2C0] transition"
+          "
         >
-          © 2025 ScrollGram. All rights reserved.
+          <span className="hover:text-[#4DF2C0] transition cursor-pointer">© 2025 ScrollGram. All rights reserved.</span>
         </p>
       </div>
     </>
